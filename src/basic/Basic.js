f9d933d5e8a3ce66b20c518a6737d41cea926a46
MAKUSref <tomaszmakowski002731@gmail.com>
2022-09-20 12:39:51 +0200

    Add examples

---
+++ src/basic/Basic.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand
+} from 'mdb-react-ui-kit';
+
+export default function App() {
+  return (
+    <>
+      <MDBNavbar light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
+        </MDBContainer>
+      </MDBNavbar>
+
+      <br />
+
+      <MDBNavbar light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand tag="span" className='mb-0 h1'>Navbar</MDBNavbarBrand>
+        </MDBContainer>
+      </MDBNavbar>
+
+      <br />
+
+      <MDBNavbar light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>
+            <img
+              src='https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.webp'
+              height='30'
+              alt=''
+              loading='lazy'
+            />
+          </MDBNavbarBrand>
+        </MDBContainer>
+      </MDBNavbar>
+
+      <br />
+
+      <MDBNavbar light bgColor='light'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand href='#'>
+            <img
+              src='https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.webp'
+              height='30'
+              alt=''
+              loading='lazy'
+            />
+            MDBootstrap
+          </MDBNavbarBrand>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
+  );
+}
